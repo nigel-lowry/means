fdad6bc9c1f8d91a1fcab2a8252ff123c0dfb39f
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2011-11-03 01:21:27 +0000

    Got arithmetic mean test passing

--- lib/Mean.rb
+++ lib/Mean.rb
@@ -1,5 +1,7 @@
 class Mean
-  def Mean.arithmetic
-
+  def Mean.arithmetic data
+    sum = 0
+    data.each {|v| sum += v}
+    sum / data.size
   end
 end
--- spec/Mean_spec.rb
+++ spec/Mean_spec.rb
@@ -4,7 +4,10 @@ describe "Mean" do
 
   describe "#arithmetic" do
     it "should be 500.5 for [1, 1000]" do 
-      Mean.arithmetic[1, 1000].should be_close(500.5, 0.05)
+      data = [1.0, 1000.0]
+      Mean.arithmetic(data).should be_close(500.5, 0.05)
     end
+
+    it "should work with integers too"
   end
 end
