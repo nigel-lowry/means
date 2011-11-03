e609d0b786c0e1078d67316a6bded8a2555ee064
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2011-11-03 14:54:13 +0000

    Reformatting and typos

--- spec/Mean_spec.rb
+++ spec/Mean_spec.rb
@@ -9,13 +9,11 @@ describe "Mean" do
 
     it "should be the singleton element of a singleton array" do
       element = 5.5
-
       Mean.arithmetic([element]).should == element
     end
 
-    it "should be the common value in a when all values are equal" do
+    it "should be the common value in an array when all values are equal" do
       element = 5.5
-
       Mean.arithmetic([element, element]).should == element
     end
 
@@ -26,19 +24,16 @@ describe "Mean" do
 
     it "should work with integers" do
       data = [1, 2]
-
       Mean.arithmetic(data).should be_within(0.01).of(1.5) 
     end
 
     it "should work with an int then a float" do
       data = [1, 2.0]
-
       Mean.arithmetic(data).should be_within(0.01).of(1.5) 
     end
 
     it "should work with a float then an int" do
       data = [1.0, 2]
-
       Mean.arithmetic(data).should be_within(0.01).of(1.5) 
     end
   end
@@ -50,40 +45,34 @@ describe "Mean" do
 
     it "should be the singleton element of a singleton array" do
         element = 5.5
-
         Mean.geometric([element]).should == element
     end
 
     it "should be the common value in a when all values are equal" do
       element = 5.5
-
       Mean.geometric([element, element]).should == element
     end
 
     it "should be zero when there is even a single zero" do
       data = [1.1, 2.2, 0.0]
-
       Mean.geometric(data).should == 0.0
     end
 
     it "should be 31.622 for [1, 1000]" do
       data = [1.0, 1000.0]
-
       Mean.geometric(data).should be_within(0.005).of(31.622)
     end
   end
 
   describe "#harmonic" do
-    it "should be be according to the formula of a two elements" do
+    it "should be be according to the formula with two elements" do
       d1, d2 = 1.0, 2.0
       data = [d1, d2]
-
       Mean.harmonic(data).should be_within(0.01).of((2.0 * d1 * d2) / (d1 + d2))
     end
 
-    it "should be the common value in a when all values are equal" do
+    it "should be the common value in an array when all values are equal" do
       element = 5.5
-
       Mean.harmonic([element, element]).should == element
     end
 
