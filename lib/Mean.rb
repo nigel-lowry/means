39999d9b4b35ca288a5a66bb45d5080a91667512
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2011-11-06 13:57:33 +0000

    Got rid of duplication from method parameter

--- lib/Mean.rb
+++ lib/Mean.rb
@@ -13,7 +13,7 @@ class Mean
     data.size / data.reduce(0) {|sum, element| sum += (1 / element)} unless data.empty?
   end
 
-  def initialize(params = {:sum => 0, :sum_of_reciprocals => 0, :product => 1, :count => 0})
+  def initialize(params = {})
     @sum = params[:sum]
     @sum_of_reciprocals = params[:sum_of_reciprocals]
     @product = params[:product]
