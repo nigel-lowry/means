ad2168c3231a84a16c7ba78fcb48b8d3ca3b746f
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2011-11-06 16:48:29 +0000

    Moved over to yard since this is what RubyGems.org supports

--- lib/Mean.rb
+++ lib/Mean.rb
@@ -3,20 +3,26 @@ require 'mathn'
 # Allows calculation of arithmetic, geometric and harmonic means.
 # Class methods allow the passing in of a data set. 
 # Alternatively an object can be created and elements can
-# be added with Mean#push
+# be added with #push
 class Mean
 
-  # Calculate the arithmetic mean of _data_
+  # Calculate the arithmetic mean
+  # @param [Array<Float>] data the data values
+  # @return [Float] the arithmetic mean
   def Mean.arithmetic(data)
     data.reduce(:+) / data.size unless data.empty?
   end
 
-  # Calculate the geometric mean of _data_
+  # Calculate the geometric mean
+  # @param (see Mean.arithmetic)
+  # @return [Float] the geometric mean
   def Mean.geometric(data)
     data.reduce(:*) ** (1 / data.size) unless data.empty?
   end
 
-  # Calculate the harmonic mean of _data_
+  # Calculate the harmonic mean
+  # @param (see Mean.arithmetic)
+  # @return [Float] the harmonic mean
   def Mean.harmonic(data)
     data.size / data.reduce(0) {|sum, element| sum += (1 / element)} unless data.empty?
   end
@@ -24,9 +30,10 @@ class Mean
   # Remember the initial state. 
   #
   # If you are passing in an initial state:
-  # [arithmetic mean] needs :sum and :count
-  # [geometric mean] needs :product and :count.
-  # [harmonic mean] needs :sum_of_reciprocals and :count
+  # [arithmetic mean] needs `:sum` and `:count`
+  # [geometric mean] needs `:product` and `:count`
+  # [harmonic mean] needs `:sum_of_reciprocals` and `:count`
+  # @param [Hash{Symbol => Fixnum}]
   def initialize(params={})
     @sum                ||= params[:sum]                ||= 0
     @sum_of_reciprocals ||= params[:sum_of_reciprocals] ||= 0
@@ -34,7 +41,8 @@ class Mean
     @count              ||= params[:count]              ||= 0
   end
 
-  # Add _element_ to the data set
+  # Add element to the data set
+  # @param [Float] element the element to add
   def push(element)
     @sum += element
     @sum_of_reciprocals += (1 / element)
@@ -42,17 +50,20 @@ class Mean
     @count += 1
   end
 
-  # Calculate the arithmetic mean of the data set
+  # Calculate the arithmetic mean
+  # @return [Float] the arithmetic mean
   def arithmetic_mean
     @sum / @count unless @count == 0
   end
 
-  # Calculate the geometric mean of the data set
+  # Calculate the geometric mean
+  # @return [Float] the geometric mean
   def geometric_mean
     @product ** (1 / @count) unless @count == 0
   end
 
-  # Calculate the harmonic mean of the data set
+  # Calculate the harmonic mean
+  # @return [Float] the harmonic mean
   def harmonic_mean
     @count / @sum_of_reciprocals unless @count == 0
   end
