caed61497230d5dfe70dec4f40880a44b33db2f3
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2012-01-02 16:13:06 +0000

    Got rid of redundant 'shoulds' in test names

--- spec/Mean_spec.rb
+++ spec/Mean_spec.rb
@@ -3,73 +3,73 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Mean" do
 
   describe "#arithmetic" do
-    it "should be nil for an empty array" do
+    it "is nil for an empty array" do
       Mean.arithmetic([]).should be_nil
     end
 
-    it "should be the singleton element of a singleton array" do
+    it "is the singleton element of a singleton array" do
       element = 5.5
       Mean.arithmetic([element]).should == element
     end
 
-    it "should be the common value in an array when all values are equal" do
+    it "is the common value in an array when all values are equal" do
       element = 5.5
       Mean.arithmetic([element, element]).should == element
     end
 
-    it "should be 1.5 for [1, 2]" do 
+    it "is 1.5 for [1, 2]" do 
       data = [1.0, 2.0]
       Mean.arithmetic(data).should be_within(0.01).of(1.5)
     end
 
-    it "should work with integers" do
+    it "works with integers" do
       data = [1, 2]
       Mean.arithmetic(data).should be_within(0.01).of(1.5) 
     end
 
-    it "should work with an int then a float" do
+    it "works with an int then a float" do
       data = [1, 2.0]
       Mean.arithmetic(data).should be_within(0.01).of(1.5) 
     end
 
-    it "should work with a float then an int" do
+    it "works with a float then an int" do
       data = [1.0, 2]
       Mean.arithmetic(data).should be_within(0.01).of(1.5) 
     end
 
-    it "should work with three numbers" do
+    it "works with three numbers" do
       data = [1, 2, 3]
       Mean.arithmetic(data).should == 2
     end
   end
 
   describe "#geometric" do
-    it "should be nil for an empty array" do
+    it "is nil for an empty array" do
       Mean.geometric([]).should be_nil
     end
 
-    it "should be the singleton element of a singleton array" do
+    it "is the singleton element of a singleton array" do
       element = 5.5
       Mean.geometric([element]).should == element
     end
 
-    it "should be the common value in a when all values are equal" do
+    it "is the common value in a when all values are equal" do
       element = 5.5
       Mean.geometric([element, element]).should == element
     end
 
-    it "should be zero when there is even a single zero" do
+    it "is zero when there is even a single zero" do
       data = [1.1, 2.2, 0.0]
       Mean.geometric(data).should == 0
     end
 
-    it "should be the square root of the product of two numbers" do
+    it "is the square root of the product of two numbers" do
       d1, d2 = 2, 8
       data = [d1, d2]
       Mean.geometric(data).should == 4
     end
 
-    it "should be the cube root of the product of three numbers" do
+    it "is the cube root of the product of three numbers" do
       d1, d2, d3 = 4, 1, 1.0 / 32.0
       data = [d1, d2, d3]
       Mean.geometric(data).should == 1.0 / 2.0
@@ -77,32 +77,32 @@ describe "Mean" do
   end
 
   describe "#harmonic" do
-    it "should be nil for an empty array" do
+    it "is nil for an empty array" do
       Mean.harmonic([]).should be_nil
     end
 
-    it "should be the singleton element of a singleton array" do
+    it "is the singleton element of a singleton array" do
         element = 5.5
         Mean.harmonic([element]).should == element
     end
 
-    it "should be the common value in a when all values are equal" do
+    it "is the common value in a when all values are equal" do
       element = 5.5
       Mean.harmonic([element, element]).should == element
     end
 
-    it "should be be according to the formula with two elements" do
+    it "is according to the formula with two elements" do
       d1, d2 = 1.0, 2.0
       data = [d1, d2]
       Mean.harmonic(data).should be_within(0.01).of((2.0 * d1 * d2) / (d1 + d2))
     end
 
-    it "should be 12/7 for [1, 2, 4]" do
+    it "is 12/7 for [1, 2, 4]" do
       data = [1.0, 2.0, 4.0]
       Mean.harmonic(data).should be_within(0.01).of(1.7142)
     end
 
-    it "should be zero if there is even one zero" do 
+    it "is zero if there is even one zero" do 
       data = [1.0, 2.0, 0.0]
       Mean.harmonic(data).should == 0
     end
@@ -113,7 +113,7 @@ describe "Mean" do
       @m = Mean.new()
     end
 
-    it "should accumulate the arithmetic mean" do
+    it "accumulates the arithmetic mean" do
       @m.arithmetic_mean.should be_nil
       @m.push 1
       @m.arithmetic_mean.should == 1
@@ -123,7 +123,7 @@ describe "Mean" do
       @m.arithmetic_mean.should == 2
     end
 
-    it "should accumulate the geometric mean" do
+    it "accumulates the geometric mean" do
       @m.geometric_mean.should be_nil
       @m.push 2
       @m.geometric_mean.should == 2
@@ -131,7 +131,7 @@ describe "Mean" do
       @m.geometric_mean.should == 4
     end
 
-    it "should accumulate the harmonic mean" do
+    it "accumulates the harmonic mean" do
       @m.harmonic_mean.should be_nil
       @m.push 1
       @m.harmonic_mean.should == 1
@@ -141,21 +141,21 @@ describe "Mean" do
   end
 
   describe "initialising accumulator" do
-    it "should resurrect the arithmetic mean using the count and sum" do
+    it "resurrects the arithmetic mean using the count and sum" do
       m = Mean.new(count: 1, sum: 1)
       m.arithmetic_mean.should == 1
       m.push 2
       m.arithmetic_mean.should == 1.5
     end
 
-    it "should resurrect the geometric mean using the count and product" do
+    it "resurrects the geometric mean using the count and product" do
       m = Mean.new(count: 1, product: 2)
       m.geometric_mean.should == 2
       m.push 8
       m.geometric_mean.should == 4
     end
 
-    it "should resurrect the harmonic mean using the count and sum of the reciprocals" do
+    it "resurrects the harmonic mean using the count and sum of the reciprocals" do
       m = Mean.new(count: 1, sum_of_reciprocals: 1)
       m.harmonic_mean.should == 1
       m.push 2
