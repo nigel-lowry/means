666fccf7fb7aa9f661f9c0527f25b87be07fd58d
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2012-03-04 14:18:36 +0000

    Change from Float to Numeric in YARD

--- lib/means.rb
+++ lib/means.rb
@@ -7,22 +7,22 @@ require 'mathn'
 class Mean
 
   # Calculate the arithmetic mean
-  # @param [Array<Float>] data the data values
-  # @return [Float] the arithmetic mean
+  # @param [Array<Numeric>] data the data values
+  # @return [Numeric] the arithmetic mean
   def Mean.arithmetic(data)
     data.reduce(:+) / data.size unless data.empty?
   end
 
   # Calculate the geometric mean
   # @param (see Mean.arithmetic)
-  # @return [Float] the geometric mean
+  # @return [Numeric] the geometric mean
   def Mean.geometric(data)
     data.reduce(:*) ** (1 / data.size) unless data.empty?
   end
 
   # Calculate the harmonic mean
   # @param (see Mean.arithmetic)
-  # @return [Float] the harmonic mean
+  # @return [Numeric] the harmonic mean
   def Mean.harmonic(data)
     data.size / data.reduce(0) {|sum, element| sum += (1 / element)} unless data.empty?
   end
@@ -33,7 +33,7 @@ class Mean
   # [arithmetic mean] needs `:sum` and `:count`
   # [geometric mean] needs `:product` and `:count`
   # [harmonic mean] needs `:sum_of_reciprocals` and `:count`
-  # @param [Hash{Symbol => Fixnum}]
+  # @param [Hash{Symbol => Numeric}]
   def initialize(params={})
     @sum                ||= params[:sum]                ||= 0
     @sum_of_reciprocals ||= params[:sum_of_reciprocals] ||= 0
@@ -42,7 +42,7 @@ class Mean
   end
 
   # Add element to the data set
-  # @param [Float] element the element to add
+  # @param [Numeric] element the element to add
   def push(element)
     @sum += element
     @sum_of_reciprocals += (1 / element)
@@ -51,19 +51,19 @@ class Mean
   end
 
   # Calculate the arithmetic mean
-  # @return [Float] the arithmetic mean
+  # @return [Numeric] the arithmetic mean
   def arithmetic_mean
     @sum / @count unless @count == 0
   end
 
   # Calculate the geometric mean
-  # @return [Float] the geometric mean
+  # @return [Numeric] the geometric mean
   def geometric_mean
     @product ** (1 / @count) unless @count == 0
   end
 
   # Calculate the harmonic mean
-  # @return [Float] the harmonic mean
+  # @return [Numeric] the harmonic mean
   def harmonic_mean
     @count / @sum_of_reciprocals unless @count == 0
   end
