02bd85a2f5b29a30c9f9e227c9851b8c162c5556
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2016-03-06 17:14:25 +0000

    Force RSpec 3 expect syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,7 @@ require 'means'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
