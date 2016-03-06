7a788a02d2864fa3ee7bb07e48a52381b5949943
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2016-03-06 17:26:47 +0000

    Convert to single-quoted strings

--- spec/support/shared_examples_for_non_arithmetic_means.rb
+++ spec/support/shared_examples_for_non_arithmetic_means.rb
@@ -1,10 +1,10 @@
-shared_examples_for "non-arithmetic means" do |mean_method|
-  it "is nil when there are any zeroes" do
+shared_examples_for 'non-arithmetic means' do |mean_method|
+  it 'is nil when there are any zeroes' do
     data = [1.1, 0.0]
     expect(Mean.send(mean_method, data)).to be_nil
   end
 
-  it "is nil when there are any negative numbers" do
+  it 'is nil when there are any negative numbers' do
     data = [1, -1]
     expect(Mean.send(mean_method, data)).to be_nil
   end
