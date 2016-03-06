725f638cec8d59fcdb3a0df2c4bb87d74ed4d5c2
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2016-03-06 17:04:17 +0000

    Convert to expect syntax but left with some warnings

--- spec/means_spec.rb
+++ spec/means_spec.rb
@@ -4,15 +4,15 @@ describe "Mean" do
   describe ".arithmetic" do
     it_behaves_like "all means", :arithmetic
 
-    specify { Mean.arithmetic([1.0, 2.0]).should be_within(0.01).of(1.5) }
-    specify { Mean.arithmetic([1, 2]).should be_within(0.01).of(1.5) }
-    specify { Mean.arithmetic([1, 2.0]).should be_within(0.01).of(1.5) }
-    specify { Mean.arithmetic([1.0, 2]).should be_within(0.01).of(1.5) }
+    specify { expect(Mean.arithmetic([1.0, 2.0])).to be_within(0.01).of(1.5) }
+    specify { expect(Mean.arithmetic([1, 2])).to be_within(0.01).of(1.5) }
+    specify { expect(Mean.arithmetic([1, 2.0])).to be_within(0.01).of(1.5) }
+    specify { expect(Mean.arithmetic([1.0, 2])).to be_within(0.01).of(1.5) }
 
-    specify { Mean.arithmetic([1, 2, 3]).should == 2 }
+    specify { expect(Mean.arithmetic([1, 2, 3])).to eq(2) }
     
-    specify { Mean.arithmetic([0, 2]).should == 1 }
-    specify { Mean.arithmetic([-1, 3]).should == 1 }
+    specify { expect(Mean.arithmetic([0, 2])).to eq(1) }
+    specify { expect(Mean.arithmetic([-1, 3])).to eq(1) }
   end
 
   describe ".geometric" do
@@ -22,13 +22,13 @@ describe "Mean" do
     it "is the square root of the product of two numbers" do
       d1, d2 = 2, 8
       data = [d1, d2]
-      Mean.geometric(data).should == Math.sqrt(d1 * d2)
+      expect(Mean.geometric(data)).to eq(Math.sqrt(d1 * d2))
     end
 
     it "is the cube root of the product of three numbers" do
       d1, d2, d3 = 4, 1, 1.0 / 32.0
       data = [d1, d2, d3]
-      Mean.geometric(data).should == Math.cbrt(d1 * d2 * d3)
+      expect(Mean.geometric(data)).to eq(Math.cbrt(d1 * d2 * d3))
     end
   end
 
@@ -39,12 +39,12 @@ describe "Mean" do
     it "is according to the formula with two elements" do
       d1, d2 = 1.0, 2.0
       data = [d1, d2]
-      Mean.harmonic(data).should be_within(0.01).of((2.0 * d1 * d2) / (d1 + d2))
+      expect(Mean.harmonic(data)).to be_within(0.01).of((2.0 * d1 * d2) / (d1 + d2))
     end
 
     it "is 12/7 for [1, 2, 4]" do
       data = [1.0, 2.0, 4.0]
-      Mean.harmonic(data).should be_within(0.01).of(12/7)
+      expect(Mean.harmonic(data)).to be_within(0.01).of(12/7)
     end
   end
 
@@ -53,61 +53,61 @@ describe "Mean" do
 
     describe "#arithmetic_mean" do
       it "accumulates the arithmetic mean" do
-        m.arithmetic_mean.should be_nil
+        expect(m.arithmetic_mean).to be_nil
         m.push 1
-        m.arithmetic_mean.should == 1
+        expect(m.arithmetic_mean).to eq(1)
         m.push 2
-        m.arithmetic_mean.should == 1.5
+        expect(m.arithmetic_mean).to eq(1.5)
         m.push 3
-        m.arithmetic_mean.should == 2
+        expect(m.arithmetic_mean).to eq(2)
         m.push 0
-        m.arithmetic_mean.should == 1.5
+        expect(m.arithmetic_mean).to eq(1.5)
         m.push -1
-        m.arithmetic_mean.should == 1
+        expect(m.arithmetic_mean).to eq(1)
       end
     end
 
     describe "#geometric_mean" do
       it "accumulates the geometric mean until zero" do
-        m.geometric_mean.should be_nil
+        expect(m.geometric_mean).to be_nil
         m.push 2
-        m.geometric_mean.should == 2
+        expect(m.geometric_mean).to eq(2)
         m.push 8
-        m.geometric_mean.should == 4
+        expect(m.geometric_mean).to eq(4)
         m.push 0
-        m.geometric_mean.should be_nil
+        expect(m.geometric_mean).to be_nil
       end
 
       it "accumulates the geometric mean until negative" do
-        m.geometric_mean.should be_nil
+        expect(m.geometric_mean).to be_nil
         m.push 2
-        m.geometric_mean.should == 2
+        expect(m.geometric_mean).to eq(2)
         m.push 8
-        m.geometric_mean.should == 4
+        expect(m.geometric_mean).to eq(4)
         m.push -1
-        m.geometric_mean.should be_nil
+        expect(m.geometric_mean).to be_nil
       end
     end
 
     describe "#harmonic_mean" do
       it "accumulates the harmonic mean until zero" do
-        m.harmonic_mean.should be_nil
+        expect(m.harmonic_mean).to be_nil
         m.push 1
-        m.harmonic_mean.should == 1
+        expect(m.harmonic_mean).to eq(1)
         m.push 2
-        m.harmonic_mean.should == 4.0 / 3.0
+        expect(m.harmonic_mean).to eq(4.0 / 3.0)
         m.push 0
-        m.harmonic_mean.should be_nil
+        expect(m.harmonic_mean).to be_nil
       end
 
       it "accumulates the harmonic mean until negative" do
-        m.harmonic_mean.should be_nil
+        expect(m.harmonic_mean).to be_nil
         m.push 1
-        m.harmonic_mean.should == 1
+        expect(m.harmonic_mean).to eq(1)
         m.push 2
-        m.harmonic_mean.should == 4.0 / 3.0
+        expect(m.harmonic_mean).to eq(4.0 / 3.0)
         m.push -1
-        m.harmonic_mean.should be_nil
+        expect(m.harmonic_mean).to be_nil
       end
     end
   end
@@ -115,23 +115,23 @@ describe "Mean" do
   describe "initialising accumulator" do
     it "resurrects the arithmetic mean using the count and sum" do
       m = Mean.new(count: 1, sum: 1)
-      m.arithmetic_mean.should == 1
+      expect(m.arithmetic_mean).to eq(1)
       m.push 2
-      m.arithmetic_mean.should == 1.5
+      expect(m.arithmetic_mean).to eq(1.5)
     end
 
     it "resurrects the geometric mean using the count and product" do
       m = Mean.new(count: 1, product: 2)
-      m.geometric_mean.should == 2
+      expect(m.geometric_mean).to eq(2)
       m.push 8
-      m.geometric_mean.should == 4
+      expect(m.geometric_mean).to eq(4)
     end
 
     it "resurrects the harmonic mean using the count and sum of the reciprocals" do
       m = Mean.new(count: 1, sum_of_reciprocals: 1)
-      m.harmonic_mean.should == 1
+      expect(m.harmonic_mean).to eq(1)
       m.push 2
-      m.harmonic_mean.should == 4.0 / 3.0
+      expect(m.harmonic_mean).to eq(4.0 / 3.0)
     end
 
     it "rejects an unknown key" do
--- spec/support/shared_examples_for_all_means.rb
+++ spec/support/shared_examples_for_all_means.rb
@@ -1,15 +1,15 @@
 shared_examples_for "all means" do |mean_method|
   it "is nil for an empty array" do
-    Mean.send(mean_method, []).should be_nil
+    expect(Mean.send(mean_method, [])).to be_nil
   end
 
   it "is the singleton element of a singleton array" do
     element = 5.5
-    Mean.send(mean_method, [element]).should == element
+    expect(Mean.send(mean_method, [element])).to eq(element)
   end
 
   it "is the common value in an array when all values are equal" do
     element = 5.5
-    Mean.send(mean_method, [element, element]).should == element
+    expect(Mean.send(mean_method, [element, element])).to eq(element)
   end
 end
